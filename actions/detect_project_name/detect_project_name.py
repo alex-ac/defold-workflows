045d13451265e94599ff6824ba8488debb014ad6
Aleksandr Derbenev <ya.alex-ac@yandex.com>
2022-08-25 11:45:52 +0100

    Detect overriden project name from settings.

--- actions/detect_project_name/detect_project_name.py
+++ actions/detect_project_name/detect_project_name.py
@@ -10,6 +10,8 @@ def run(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('project', type=pathlib.Path,
                         help='Path to the defold project.')
+    parser.add_argument('--settings', default=None,
+                        help='Comma-separated list of settings files.')
     parser.add_argument('--append-to', type=pathlib.Path,
                         help='Path to the env file.')
     args = parser.parse_args(argv)
@@ -18,6 +20,18 @@ def run(argv=None):
     config = configparser.ConfigParser()
     config.read(project_file, encoding='utf-8')
     project_name = config.get('project', 'title')
+    print(f'Loaded project.title from {project_file}: {project_name}')
+
+    if args.settings is not None:
+        for path in args.settings.split(','):
+
+            config_path = pathlib.Path(path)
+            config = configparser.ConfigParser()
+            config.read(config_path, encoding='utf-8')
+            if config.has_option('project', 'title'):
+                project_name = config.get('project', 'title')
+                print(
+                    f'Loaded project.title from {config_path}: {project_name}')
 
     print(f'::set-output name=project_name={project_name}')
 
